7a1f6e1939df9a005fa1bb6ffbba53480350a641
malongdre <golumposey@gmail.com>
2023-07-31 13:18:33 +0200

    Adding mentions.

--- src/layout/footer/Footer.jsx
+++ src/layout/footer/Footer.jsx
@@ -4,9 +4,17 @@ function  Footer() {
     return (
         <footer>
             <div className={styles.footerContainer}>
-                <p className={styles.footerName}>juststop.dev</p>
-                <p>contact@juststop.dev</p>
-                <p>© JustStop__ 2023. All rights reserved.</p>
+                <div className={styles.leftFooter}>
+                    <p className={styles.footerName}>juststop.dev</p>
+                    <p>contact@juststop.dev</p>
+                    <p>© JustStop__ 2023. All rights reserved.</p>
+                </div>
+                <div className={styles.rightFooter}>
+                    <p>Inspiration: <br /><a href="https://en-zo.dev">en-zo.dev</a>
+                    <br />
+                    <a href="https://stacksorted.com/">https://stacksorted.com/</a>
+                    </p>
+                </div>
             </div>
         </footer>
     )
